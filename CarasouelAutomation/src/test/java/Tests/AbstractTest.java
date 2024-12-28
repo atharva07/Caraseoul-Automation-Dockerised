3a1bbeacdfd7ce2c333a124fc4884f28b66a3d45
atharva07 <atharva.hiwase07@gmail.com>
2024-12-28 19:11:44 +0530

    yaml file configured

--- CarasouelAutomation/src/test/java/Tests/AbstractTest.java
+++ CarasouelAutomation/src/test/java/Tests/AbstractTest.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
